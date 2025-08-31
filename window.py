033142e11679399cc33e46a19a9f810a187c42f4
michasssada <michaslate@gmail.com>
2025-08-31 17:27:38 +0200

    added signal strength label

--- window.py
+++ window.py
@@ -1,11 +1,16 @@
-from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QLabel
 
 
 class window(QWidget):
-    layout = QVBoxLayout
     def __init__(self):
         super().__init__()
+        self.layout = QVBoxLayout()
         self.setWindowTitle("Hoverbot app")
         screen = QApplication.primaryScreen().size()
         self.setGeometry(100, 100, screen.width(), screen.height())
+        self.signalStrengthLabel = QLabel(f"Signal Strength: {0}")
+        self.layout.addWidget(self.signalStrengthLabel)
+        self.setLayout(self.layout)
+
+
 
